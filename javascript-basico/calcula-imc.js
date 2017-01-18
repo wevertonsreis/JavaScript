1b2bdd76d50d9b0d7bfbd9c75481ae44cabb5358
Weverton Reis <weverton.sant.reis@gmail.com>
2017-01-17 23:07:39 -0200

    mais um exercicio

--- javascript-basico/calcula-imc.js
+++ javascript-basico/calcula-imc.js
@@ -1,15 +1,28 @@
 
-var peso = 50;
-var altura = 0;
+var nome = document.getElementById("nome-1").textContent;
+var peso = document.getElementById("peso-1").textContent;
+var altura = document.getElementById("altura-1").textContent;
 
+console.log(nome);
 console.log(peso);
 console.log(altura);
 
-if (altura > 0) {
+var passiente = {
+	nome : nome,
+	peso : peso,
+	altura : altura
+};
 
-	var imc = peso / (altura * altura);
-	console.log(imc);	
+var idDoImc = document.getElementById("imc-1");
 
+if (passiente.altura > 0) {
+
+	var imc = passiente.peso / (passiente.altura * passiente.altura);
+	
+	console.log(imc);
+
+	idDoImc.textContent = imc;
+	
 } else {
 	console.log("Altura invalida para o calculo do IMC.");
 }
